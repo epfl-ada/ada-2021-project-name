bbfac3be09285f21987b409776561349c692c804
maximzubkov <zubkov.md@phystech.edu>
2021-12-16 22:09:46 +0100

    Split notebook on files

---
+++ postprocessing/filter.py
@@ -0,0 +1,51 @@
+def filter_irrelevant(x):
+    return (" a bit " not in x) and \
+        (" little bit " not in x) and \
+        (" least bit concerned " not in x) and \
+        (" every bit confident " not in x) and \
+        (" the least bit coincidental " not in x) and \
+        ("bitching " not in x) and \
+        ("bitching " not in x) and \
+        (" bitching" not in x) and \
+        ("bitchin " not in x) and \
+        ("bitchin " not in x) and \
+        (" bitchin" not in x) and \
+        (" cryptus " not in x) and \
+        (" cryptus" not in x) and \
+        ("cryptus " not in x) and \
+        (" cryptic " not in x) and \
+        (" cryptic" not in x) and \
+        ("cryptic " not in x) and \
+        (" cryptum " not in x) and \
+        (" cryptum" not in x) and \
+        ("cryptum " not in x) and \
+        (" cryptex " not in x) and \
+        (" cryptex" not in x) and \
+        ("cryptex " not in x) and \
+        (" cryptoriana " not in x) and \
+        (" cryptoriana" not in x) and \
+        ("cryptoriana " not in x) and \
+        (" cryptonazi " not in x) and \
+        (" cryptonazi" not in x) and \
+        ("cryptonazi " not in x) and \
+        (" crypto-nazi " not in x) and \
+        (" crypto-nazi" not in x) and \
+        ("crypto-nazi " not in x) and \
+        (" crypto nazi " not in x) and \
+        (" crypto nazi" not in x) and \
+        ("crypto nazi " not in x) and \
+        (" cryptozoology " not in x) and \
+        (" cryptozoology" not in x) and \
+        ("cryptozoology " not in x) and \
+        (" cryptozoologi " not in x) and \
+        (" cryptozoologi" not in x) and \
+        ("cryptozoologi " not in x) and \
+        (" cryptochrome " not in x) and \
+        (" cryptochrome" not in x) and \
+        ("cryptochrome " not in x) and \
+        (" bitbucket " not in x) and \
+        (" bitbucket" not in x) and \
+        ("bitbucket " not in x) and \
+        (" bitwarden " not in x) and \
+        (" bitwarden" not in x) and \
+        ("bitwarden " not in x)
